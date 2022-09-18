4513409c07b207003ffd6108077c672689d7876a
M.Shirisha <71245646+shirisha1128@users.noreply.github.com>
2022-09-18 12:07:08 +0530

    Add files via upload

---
+++ MainAttendance.py
@@ -0,0 +1,110 @@
+import cv2
+import os
+import numpy as np
+from datetime import datetime
+import face_recognition
+from tensorflow.keras.models import model_from_json
+
+root_dir = os.getcwd()
+# Load Face Detection Model
+face_cascade = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
+# Load Anti-Spoofing Model graph
+json_file = open('antispoofing_models/antispoofing_model.json','r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+# load antispoofing model weights 
+model.load_weights('antispoofing_models/antispoofing_model.h5')
+print("Model loaded from disk")
+ 
+path = 'images'
+images = []
+personNames = []
+myList = os.listdir(path)
+print(myList)
+for cu_img in myList:
+    current_Img = cv2.imread(f'{path}/{cu_img}')
+    images.append(current_Img)
+    personNames.append(os.path.splitext(cu_img)[0])
+print(personNames)
+
+def faceEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def attendance(name):
+    with open('attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            time_now = datetime.now()
+            tStr = time_now.strftime('%H:%M:%S')
+            dStr = time_now.strftime('%d/%m/%Y')
+            f.writelines(f'\n{name},{dStr},{tStr}')
+
+
+encodeListKnown = faceEncodings(images)
+print('All Encodings Complete!!!')
+
+video = cv2.VideoCapture(0)
+while True:
+    try:
+        ret,frame = video.read()
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray,1.3,5)
+        for (x,y,w,h) in faces:  
+            face = frame[y-5:y+h+5,x-5:x+w+5]
+            resized_face = cv2.resize(face,(160,160))
+            resized_face = resized_face.astype("float") / 255.0
+            # resized_face = img_to_array(resized_face)
+            resized_face = np.expand_dims(resized_face, axis=0)
+            # model to determine if the face is "real" or "fake"
+            preds = model.predict(resized_face)[0]
+            print(preds)
+            if preds> 0.4:
+                
+                label = 'spoof'
+                cv2.putText(frame, label, (x,y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+                cv2.rectangle(frame, (x, y), (x+w,y+h),(0, 0, 255), 2)
+            else:
+                label = 'real'
+                cv2.putText(frame, label, (x,y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+                #cv2.rectangle(frame, (x, y), (x+w,y+h),(0, 255, 0), 2)
+                faces = cv2.resize(frame,(0, 0),None,0.25,0.25)
+                faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
+                facesCurrentFrame = face_recognition.face_locations(faces)
+                encodesCurrentFrame = face_recognition.face_encodings(faces, facesCurrentFrame)
+
+                for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
+                    matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+                    faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+                    # print(faceDis)
+                    matchIndex = np.argmin(faceDis)
+
+                if matches[matchIndex]:
+                    name = personNames[matchIndex].upper()
+                    #print(name)
+                    y1, x2, y2, x1 = faceLoc
+                    y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.rectangle(frame, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+                    attendance(name)
+
+        cv2.imshow("Camera", frame)
+        if cv2.waitKey(1) == 13:
+         break
+    except Exception as e:
+        pass
+video.release()        
+cv2.destroyAllWindows()
